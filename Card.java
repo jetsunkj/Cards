6732fbab75956962943ff8fc5bcc31806d25c26a
jetsunkj <50220203+jetsunkj@users.noreply.github.com>
2019-05-14 15:12:58 -0700

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Card  {
+    int myRank;
+    int mySuit;
+    
+    
+    public Card(int myRank, int mySuit){
+        this.myRank = myRank;
+        this.mySuit = mySuit;
+        
+        
+    }
+    
+    public int rank() {
+        return myRank;
+    }
+
+    public int suit() {
+        return mySuit;
+    }
+
+}
