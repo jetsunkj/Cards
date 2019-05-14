39e722da7a8de43a419a2000972183883fd43840
jetsunkj <50220203+jetsunkj@users.noreply.github.com>
2019-05-14 15:13:41 -0700

    Create Canvas.java

---
+++ Canvas.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.awt.Canvas.*;
+import java.lang.Object;
+import java.awt.Component;
+public class Canvas
+{
+   
+    public Canvas()
+    {
+        Canvas.setSize(400, 400);
+        
+    }
+
+}
