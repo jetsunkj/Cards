cce470b756896f2bf7428587b55ac42da4b52266
jetsunkj <50220203+jetsunkj@users.noreply.github.com>
2019-05-23 15:39:24 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,17 +1,36 @@
 import java.util.*;
-
-
-public class Game extends Deck
+import java. awt. Graphics;
+import javax. swing. JFrame;
+import javax. swing. JPanel;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+public class Game extends JPanel
 {
-   
+    private int myWindowWidth = 700;
+    private int myWindowHeight = 700;
     public Game()
     {
-      
+        Deck d= new Deck();
+        Player p= new Player();
+        Table t= new Table();
+        
+        
+        JFrame easel = new JFrame();	
+        easel.setSize (myWindowWidth, myWindowHeight);
+        easel.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        easel.add (this);
+        easel.setVisible (true);
+        
+
+    }
+    
+    public void paintComponent(Graphics g){
+         	
     }
     
 
-    public void placeCard(){
-        
+    public boolean IsConsecutive(){
+        return true;
     }
 
 }
