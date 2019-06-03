2bc64d04724adba9aae07921b350aaecda57de48
jetsunkj <50220203+jetsunkj@users.noreply.github.com>
2019-06-03 14:57:39 -0700

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,17 +1,21 @@
 import java.util.*;
-public class Table extends Deck
+public class Table 
 {
-    public ArrayList tableCard= new ArrayList<Card>();
-    public ArrayList tableCard2= new ArrayList<Card>();
-    public ArrayList stack= new ArrayList<Card>();
-    public ArrayList stack2= new ArrayList<Card>();
+    public ArrayList<Card>tableCard= new ArrayList<Card>();
+    public ArrayList<Card> tableCard2= new ArrayList<Card>();
+    public ArrayList<Card> stack= new ArrayList<Card>();
+    public ArrayList<Card> stack2= new ArrayList<Card>();
+    public Player p= new Player();
+    Deck d = new Deck();
+    
     public Table()
     {
-        fill(tableCard, 5);
-        fill(tableCard2, 5);
-        fill(stack, 15);
-        fill(stack, 15);
-
+        d.fill(tableCard, 1);
+        d.fill(tableCard2, 1);
+        d.fill(stack, 5);
+        d.fill(stack2, 5);
+        p.fillHand(d);
+        
     }
 
    
